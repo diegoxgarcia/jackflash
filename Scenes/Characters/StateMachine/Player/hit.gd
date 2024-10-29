22da6b9d6274998bab8a34d644820975ae39ee4b
diegoxgarcia <diegoxgarcia@gmail.com>
2024-10-29 01:00:48 -0300

    Add base for Player FSM

---
+++ Scenes/Characters/StateMachine/Player/hit.gd
@@ -0,0 +1,2 @@
+class_name Hit
+extends State
---
+++ Scenes/Characters/StateMachine/state.gd
@@ -0,0 +1,25 @@
+class_name State
+extends Node
+
+signal transitioned
+
+func physics_update(delta : float):
+	pass
+	
+func update(delta : float):
+	pass
+	
+func enter():
+	pass
+
+func exit():
+	pass
+	
+func change_direction_velocity(player : Player, delta : float, direction : float):
+	#player.velocity.x = direction * player.player_data.speed * delta
+	#if direction > 0:
+		#player.visual.scale.x = -1
+	#else:
+		#player.visual.scale.x = 1
+#	 TODO chequear que esten los 3 ejes 
+	pass
