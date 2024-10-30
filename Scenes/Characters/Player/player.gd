f99a7239f667fccf6deb78d7802b542c12a3e5c9
diegoxgarcia <diegoxgarcia@gmail.com>
2024-10-30 18:36:46 -0300

    WIP Player test

--- Scenes/Characters/Player/player.gd
+++ Scenes/Characters/Player/player.gd
@@ -4,16 +4,20 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+@onready var animation_player = $Visual/jackflash/AnimationPlayer
+@onready var jackflash = $Visual/jackflash
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animation_player.play("Fall")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animation_player.play("JumpAndStay")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,8 +26,13 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		animation_player.play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		animation_player.play("Idle")
+		
+	if direction:
+		jackflash.rotation.y = atan2(direction.x, direction.z)
 
 	move_and_slide()
