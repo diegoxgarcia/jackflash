b47328e2cea2c5152cafd2241e4ecdf7e7bb6efe
diegoxgarcia <diegoxgarcia@gmail.com>
2024-12-11 23:59:03 -0300

    WIP

--- Scenes/Menu/credits_menu.gd
+++ Scenes/Menu/credits_menu.gd
@@ -2,14 +2,10 @@ class_name Credits
 extends Control
 
 var vbox_aux : VBoxContainer
+var label_aux : Label
 @onready var animation_player = $AnimationPlayer
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel") && vbox_aux:
-		self.hide()
-		vbox_aux.show()
-		animation_player.stop()
-	
 	if Input.is_action_pressed("jump"):
 		animation_player.speed_scale = 10
 	else:
@@ -17,9 +13,11 @@ func _process(delta):
 	pass
 
 
-func show_hide_play(vbox : VBoxContainer):
+func show_hide_play(vbox : VBoxContainer, recommends_label : Label):
 	vbox_aux = vbox
+	label_aux = recommends_label
 	vbox.hide()
+	recommends_label.hide()
 	show()
 	animation_player.play("slide_up_credits")
 	pass
@@ -30,4 +28,5 @@ func _on_animation_player_animation_finished(anim_name):
 		"slide_up_credits":
 			self.hide()
 			vbox_aux.show()
+			label_aux.show()
 	pass
--- Scenes/Menu/game_menu.gd
+++ Scenes/Menu/game_menu.gd
@@ -4,7 +4,8 @@ extends Control
 @onready var v_box_container = $VBoxContainer
 @onready var credits_menu = $CreditsMenu
 @onready var how_to_play_menu = $HowToPlayMenu
-	
+@onready var recommends_label = $RecommendsLabel
+
 func _on_start_game_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
 	pass
@@ -18,5 +19,5 @@ func _on_how_to_play_button_pressed():
 	pass 
 
 func _on_credit_button_pressed():
-	credits_menu.show_hide_play(v_box_container)
+	credits_menu.show_hide_play(v_box_container, recommends_label)
 	pass 
